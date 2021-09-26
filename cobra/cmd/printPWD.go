7eebb6d6af5f2f64aa7283132e4c48f5ae507abe
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-09-26 20:50:24 +0800

    cobra

---
+++ cobra/cmd/printPWD.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var printPWDCmd = &cobra.Command{
+	Use:                        "pwd",
+	Aliases:                    nil,
+	SuggestFor:                 nil,
+	Short:                      "print pwd",
+	Long:                       "print pwd",
+	Example:                    "",
+	ValidArgs:                  nil,
+	ValidArgsFunction:          nil,
+	Args:                       nil,
+	ArgAliases:                 nil,
+	BashCompletionFunction:     "",
+	Deprecated:                 "",
+	Annotations:                nil,
+	Version:                    "",
+	PersistentPreRun:           nil,
+	PersistentPreRunE:          nil,
+	PreRun:                     nil,
+	PreRunE:                    nil,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(os.Getwd())
+	},
+	RunE:                       nil,
+	PostRun:                    nil,
+	PostRunE:                   nil,
+	PersistentPostRun:          nil,
+	PersistentPostRunE:         nil,
+	FParseErrWhitelist:         cobra.FParseErrWhitelist{},
+	CompletionOptions:          cobra.CompletionOptions{},
+	TraverseChildren:           false,
+	Hidden:                     false,
+	SilenceErrors:              false,
+	SilenceUsage:               false,
+	DisableFlagParsing:         false,
+	DisableAutoGenTag:          false,
+	DisableFlagsInUseLine:      false,
+	DisableSuggestions:         false,
+	SuggestionsMinimumDistance: 0,
+}
+
+func init(){
+	rootCmd.AddCommand(printPWDCmd)
+}
